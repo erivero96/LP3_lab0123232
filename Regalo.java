aea0d150a1ef06c03489cd9f987892bbb3f34cb5
erivero96 <108624162+erivero96@users.noreply.github.com>
2023-08-15 23:31:08 -0500

    Create Regalo.java

---
+++ Regalo.java
@@ -0,0 +1,12 @@
+public class Regalo {
+  public static String elegir(int vida) {
+    String gift  = " ";
+    switch (vida) {
+    case 1: gift = "Un pasaje al caribe"; break;
+    case 2: gift = "Una visita al museo mas cercano a tu casa"; break;
+    case 3: gift = "Una entrada al cine"; break;
+    }
+    return gift;
+  }
+}
+  
